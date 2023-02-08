900c227cfc953050736bb5064f53e2444d757266
spekles <spekles@spekles-VirtualBox>
2023-02-08 07:58:44 +0200

    First push

---
+++ TaskServlets/src/main/java/com/servlets/PersonalServlet.java
@@ -0,0 +1,84 @@
+package com.servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class PersonalServlet
+ */
+@WebServlet("/PersonalServlet")
+public class PersonalServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public PersonalServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		
+		response.setContentType("text/html");
+        PrintWriter printWriter = response.getWriter();
+        
+        String text = request.getParameter("text");
+        try
+        {
+        	printWriter.println("<h1>Text: " + text + "</h1>");
+        }
+        finally
+        {
+        	printWriter.close();
+        }
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		
+		response.setContentType("text/html");
+        PrintWriter printWriter = response.getWriter();
+        
+        String name = request.getParameter("username");
+        String pass = request.getParameter("userpass");
+        
+        try
+        {
+        	printWriter.println("<p>Name: " + name + "</p>");
+        	printWriter.println("<p>Password: " + pass + "</p>");
+        }
+        finally
+        {
+        	printWriter.close();
+        }
+	}
+
+	/**
+	 * @see HttpServlet#doPut(HttpServletRequest, HttpServletResponse)
+	 */
+	protected void doPut(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see HttpServlet#doDelete(HttpServletRequest, HttpServletResponse)
+	 */
+	protected void doDelete(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+}
